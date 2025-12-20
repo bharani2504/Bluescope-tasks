71262cf388a90b56d2760919db38fed73cd1621f
Your Name <bharanidharan963@gmail.com>
2025-12-20 12:24:01 +0530

    added filehandling/src project

--- src/WriteExcel.java
+++ src/WriteExcel.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.io.File;
 import java.time.LocalDate;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -33,3 +34,37 @@ public class WriteExcel {
     }
 }
 
+class ex{
+
+
+    public  static  void  main (String[] args) throws IOException {
+
+        FileInputStream FS=new FileInputStream("C:\\Users\\bhara\\Downloads\\employees.xlsx");
+        Workbook book=new XSSFWorkbook(FS);
+
+        Sheet sc=book.getSheetAt(0);
+
+        book.close();
+
+        BufferedWriter br= new BufferedWriter(new FileWriter("Output.csv"));
+        String[]lines= new String[]{sc.toString()};
+
+
+        for(String l : lines){
+            l=l.trim();
+
+            if(!l.isEmpty()){
+                l=l.replaceAll(" +" ," ");
+                br.write(l);
+                br.flush();
+            }
+        }
+        br.close();
+
+        System.out.println("Excel Successfully converted to CSV file");
+
+    }
+
+
+
+        }
